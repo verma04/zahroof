d8cecd3f514efa0cf2cf9f49b4d7a218f4c88058
Pankaj <pankaj.k@amakein.com>
2021-07-02 20:44:52 +0530

    asas

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import Home from '../components/HomePage/Home'
 import withApollo from '../hoc/withApollo'
-// import { useGetHomePage } from '@/apollo/actions'
+
 import { getDataFromTree } from '@apollo/react-ssr';
 
 
 
 
  const homePage = () => {
-    // const { data } = useGetHomePage();
-
-    // const home = data && data.page  || [];
-
-       
-    // console.log(home)
+   
     
  
     
