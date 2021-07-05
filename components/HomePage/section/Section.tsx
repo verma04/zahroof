bc33ddc0922a08712c325a6f2809126f1560f94e
Munish Dhiman <munishdhiman9@gmail.com>
2021-07-05 13:45:30 +0530

    mobiledevice

--- components/HomePage/section/Section.tsx
+++ components/HomePage/section/Section.tsx
@@ -1,104 +1,142 @@
 
 import React from 'react'
-import { Section} from './Style'
+import { Section } from './Style'
 import Image from 'next/image'
-import  Navbar from '../../layout/navbar/navbar'
+import Navbar from '../../layout/navbar/navbar'
 import { Parallax, Background } from "react-parallax";
+import { isMobile } from 'react-device-detect';
+import { isBrowser } from 'react-device-detect';
 const image1 =
   "https://res.cloudinary.com/dzcmadjl1/image/upload/v1624859749/cawdyff1kgtdbfyrmxcu.jpg";
 
-  interface Section {
-   data: {
-      firstLeftHeading: string,
-      firstLeftIcon: string,
-      firstLeftTitle: string,
-      firstLeftBgimage: string,
-      firstRightBgimage: string,
-      firstRightHeading: string,
-      firstRightIcon: string,
-      firstRightTitle: string,
-   }, // Change the required prop to an optional prop.
- }
-  const image2 =
+interface Section {
+  data: {
+    firstLeftHeading: string,
+    firstLeftIcon: string,
+    firstLeftTitle: string,
+    firstLeftBgimage: string,
+    firstRightBgimage: string,
+    firstRightHeading: string,
+    firstRightIcon: string,
+    firstRightTitle: string,
+  }, // Change the required prop to an optional prop.
+}
+const image2 =
   "https://res.cloudinary.com/dzcmadjl1/image/upload/v1624859749/sdouzbuocrwq67wffz95.jpg";
-  interface OptionalMiddleName {
-    homebannercontent: string;
-    honebannerimageurl: string;
+interface OptionalMiddleName {
+  homebannercontent: string;
+  honebannerimageurl: string;
 
 }
- 
-  const HeroSection: React.FC<Section> = (data) => {
-    return (
-      
-        <Section>
-        
-           
-           <div className="section" >
-               <div className="section_top" >
-               <div className="section_top_left" >
 
-                   <span  dangerouslySetInnerHTML={{ __html: data.data.firstLeftHeading }}   /> 
-                   <div className="tringle" ></div>
-                   </div>
-                   <div className="section_top_right" >
-                   <span  dangerouslySetInnerHTML={{ __html: data.data.firstRightHeading }}   /> 
-                   <div className="tringle" ></div>
-                   </div>
-           </div>
-              
-               <div  className="section_bottom" >
-               <Parallax bgImage={data.data.firstLeftBgimage  } strength={500}>
-               <div  className="section_bottom_set" >
-           
-               <div className="section_bottom_data" >
+const HeroSection: React.FC<Section> = (data) => {
+  return (
+
+    <Section>
+
+      {isMobile ? 
+        (<section className="section-mobile">
+          <div className="section_mobile_left" >
+
+            <div className="section_top_left" >
+              <span dangerouslySetInnerHTML={{ __html: data.data.firstLeftHeading }} />
+              <div className="tringle" ></div>
+            </div>
+
+            <Parallax bgImage={data.data.firstLeftBgimage} strength={500}>
+              <div className="section_bottom_set" >
+                <div className="section_bottom_data" >
+                  <li> <Image objectFit="contain" layout="fill" src={data.data.firstLeftIcon} alt="sd" /> </li>
+                  <li><h3>{data.data.firstLeftTitle}</h3></li>
+                  <li> <Image objectFit="contain" layout="fill" src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1623825001/kphxeenbazcwieocomkx.png' alt="" /></li>
+                </div>
+              </div>
+            </Parallax>
+
+          </div>
+          <div className="section_mobile_right" >
+
+            <div className="section_top_right" >
+              <span dangerouslySetInnerHTML={{ __html: data.data.firstRightHeading }} />
+              <div className="tringle" ></div>
+            </div>
+
+            <Parallax bgImage={data.data.firstRightBgimage} strength={500}>
+              <div className="section_bottom_set" >
+                <div className="section_bottom_data" >
+                  <li> <Image objectFit="contain" layout="fill" src={data.data.firstRightIcon} alt="sd" /></li>
+                  <li><h3>{data.data.firstRightTitle}</h3></li>
+                  <li> <Image objectFit="contain" layout="fill" src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1623825001/kphxeenbazcwieocomkx.png' alt="" /> </li>
+                </div>
+              </div>
+            </Parallax>
+
+          </div>
+        </section>) :
+       (
+          <div className="section" >
+            <div className="section_top" >
+              <div className="section_top_left" >
+
+                <span dangerouslySetInnerHTML={{ __html: data.data.firstLeftHeading }} />
+                <div className="tringle" ></div>
+              </div>
+              <div className="section_top_right" >
+                <span dangerouslySetInnerHTML={{ __html: data.data.firstRightHeading }} />
+                <div className="tringle" ></div>
+              </div>
+            </div>
+
+            <div className="section_bottom" >
+              <Parallax bgImage={data.data.firstLeftBgimage} strength={500}>
+                <div className="section_bottom_set" >
+
+                  <div className="section_bottom_data" >
                     <li>
-                    <Image     objectFit="contain" layout="fill" src={data.data.firstLeftIcon} alt="sd" />
+                      <Image objectFit="contain" layout="fill" src={data.data.firstLeftIcon} alt="sd" />
                     </li>
-                  
+
                     <li><h3>{data.data.firstLeftTitle}</h3></li>
-                 
-                    <li>
-                    <Image     objectFit="contain" layout="fill" src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1623825001/kphxeenbazcwieocomkx.png' alt="" />
-                    </li>
-                   
-                </div>
-              
 
-                   </div>
-                 </Parallax>
-                 <Parallax bgImage={data.data.firstRightBgimage  } strength={500}>
-               <div  className="section_bottom_set" >
-           
-               <div className="section_bottom_data" >
                     <li>
-                    <Image     objectFit="contain" layout="fill" src={data.data.firstRightIcon} alt="sd" />
+                      <Image objectFit="contain" layout="fill" src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1623825001/kphxeenbazcwieocomkx.png' alt="" />
                     </li>
-                  
+
+                  </div>
+
+
+                </div>
+              </Parallax>
+              <Parallax bgImage={data.data.firstRightBgimage} strength={500}>
+                <div className="section_bottom_set" >
+
+                  <div className="section_bottom_data" >
+                    <li>
+                      <Image objectFit="contain" layout="fill" src={data.data.firstRightIcon} alt="sd" />
+                    </li>
+
                     <li><h3>{data.data.firstRightTitle}</h3></li>
-                 
-                    <li>
-                    <Image     objectFit="contain" layout="fill" src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1623825001/kphxeenbazcwieocomkx.png' alt="" />
-                    </li>
-                   
-                </div>
-              
 
-                   </div>
-                 </Parallax>
-              
-              
-              
-               </div>
-              
-        
-        
-   
-       
-           </div>
-            
-        </Section>
- 
- )
+                    <li>
+                      <Image objectFit="contain" layout="fill" src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1623825001/kphxeenbazcwieocomkx.png' alt="" />
+                    </li>
+
+                  </div>
+
+
+                </div>
+              </Parallax>
+
+
+
+            </div>
+          </div>
+       )
+      }
+
+    </Section>
+
+  )
 }
 
 export default HeroSection
@@ -112,7 +150,7 @@ export default HeroSection
 // const Welcome: React.FC<WelcomeProps> = (data) => {
 
 //    console.log(data.data)
-   
+
 //  return <h1>Hello, </h1>;
 // }
 
