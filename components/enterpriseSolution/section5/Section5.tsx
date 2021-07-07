6bd850480ffd87ca82b33aeb3298dd294fcd3ad1
Munish Dhiman <munishdhiman9@gmail.com>
2021-07-07 14:17:13 +0530

    s

--- components/enterpriseSolution/section5/Section5.tsx
+++ components/enterpriseSolution/section5/Section5.tsx
@@ -17,35 +17,6 @@ interface SectionProps {
         
            
            <div className="section" >
-<<<<<<< HEAD
-               
-
-               <div className="section_top" >
-             
-               <div className="section2_data" >  <div className="section2_data_left"  >
-
-<h2>So what is this amazing 
-technology you ask?
- </h2>
-<p>
-It might surprise you. In fact, it might be  something you haven’t considered at all. But  fact is, until we turned the industry on its ear, <br/> compressor valves had been designed the  same way for over 70 years. 
-</p>
-
-</div>
- 
-
-       
-             
-     
-                   
-                   </div>
-                   
-             
-              
-               </div>
-         
-        
-=======
                 <div className="section_top" >             
                     <div className="section2_data" > 
                         <div className="section2_data_left"  >
@@ -55,7 +26,6 @@ It might surprise you. In fact, it might be  something you haven’t considered 
                         </div>    
                     </div>
                 </div>
->>>>>>> Munish-Dhiman
            </div>
             
         </Section>
