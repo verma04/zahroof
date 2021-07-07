ac4732abb42e32a5ed92560efcf1d387cf7ae120
Pankaj <pankaj.k@amakein.com>
2021-07-07 19:41:58 +0530

    assa

--- components/HomePage/makecall/makecall.tsx
+++ components/HomePage/makecall/makecall.tsx
@@ -1,58 +1,72 @@
-import React from "react";
-import { Section } from "./Style";
-import Image from "next/image";
+import React from 'react'
+import { Section} from './Style'
+import Image from 'next/image'
 
-const HeroSection: React.FC<{}> = () => {
-  return (
-    <Section>
-      <div className="section">
-        <div className="section_data">
-          <div className="section_left">
-            <div className="section-left-top">
-              <h2>MAKE THE CALL</h2>
 
-              <h3>Learn how one great decision can make a huge difference.</h3>
+ const HeroSection: React.FC<{}> = () => {
+    return (
+        <Section>
+        
+           
+           <div className="section" >
+             
+           
+             
+
+             
+               <div className="section_data" >
+<div className="section_left" >
+
+    <div className="section-left-top" >
+
+        <h2>MAKE THE CALL</h2>
+
+        <h3>
+        Learn how one great decision can make a huge difference.
+
+        </h3>
+        </div>
+
+  
+    
+    </div>
+
+    <div className="section_right" >
+    <div className="icon">
+
+        <ul>
+            <li>
+            <Image     objectFit="contain" layout="fill" src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1624874075/pnqbtnq5yxgu1o7pyquc.png' alt="sd" />
+            </li>
+            <li>Tel</li>
+            <li>(713)554-2678</li>
+        </ul>
+
+        <ul>
+            <li>  <Image     objectFit="contain" layout="fill" src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1624874075/nt4lgdaec6ngnwwo8bro.png' alt="sd" /></li>
+            <li>Email</li>
+            <li>info@zahroofvalues.com</li>
+        </ul>
+      
             </div>
-          </div>
+    <div className="form">
+        <form>
+            <div className="input-field" >
 
-          <div className="section_right">
-            <div className="icon">
-              <ul>
-                <li>
-                  <Image
-                    objectFit="contain"
-                    layout="fill"
-                    src="https://res.cloudinary.com/dzcmadjl1/image/upload/v1624874075/pnqbtnq5yxgu1o7pyquc.png"
-                    alt="sd"
-                  />
-                </li>
-                <li>Tel</li>
-                <li>(713)554-2678</li>
-              </ul>
+            <div className="field" >
+               
+         <input placeholder="Your Name" ></input>
+                </div>
+                <div className="field" >
+               
+         <input  placeholder="Your Email" ></input>
+                </div>
 
-              <ul>
-                <li>
-                  {" "}
-                  <Image
-                    objectFit="contain"
-                    layout="fill"
-                    src="https://res.cloudinary.com/dzcmadjl1/image/upload/v1624874075/nt4lgdaec6ngnwwo8bro.png"
-                    alt="sd"
-                  />
-                </li>
-                <li>Email</li>
-                <li>info@zahroofvalues.com</li>
-              </ul>
             </div>
-            <div className="form">
-              <form>
-                <div className="input-field">
-                  <div className="field">
-                    <input placeholder="Your Name"></input>
-                  </div>
-                  <div className="field">
-                    <input placeholder="Your Email"></input>
-                  </div>
+            <div className="input-field" >
+            <div className="field" >
+                
+         <input  placeholder="Subject" ></input>
                 </div>
                 <div className="field" >
                 
@@ -63,24 +77,38 @@ const HeroSection: React.FC<{}> = () => {
                     <option value="3">3</option>
                 </select>
                 {/* <input  placeholder="Departmnet" ></input> */}
-
                 </div>
 
-                <div className="input-field-1">
-                  <div className="field">
-                    <textarea placeholder="Message"></textarea>
-                  </div>
                 </div>
-                <div className="input-field">
-                  <button>Submit Message</button>
+           
+               
+                <div className="input-field-1" >
+
+                <div className="field" >
+                
+         <textarea  placeholder="Message"></textarea>
                 </div>
-              </form>
+                
+                </div>
+                <div className="input-field" >
+                <button>Submit Message</button>
+                </div>
+                
+
+        </form>
+            
             </div>
-          </div>
-        </div>
-      </div>
-    </Section>
-  );
-};
+    </div>
 
-export default HeroSection;
+                   </div>
+                   
+              
+               </div>
+         
+           
+            
+        </Section>
+    )
+}
+
+export default HeroSection
