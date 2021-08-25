fb0bbe93b480422a0bde68f0214967870061c1ff
Pankaj Verma <pankaj.k@makein.com>
2021-08-25 18:11:41 +0530

    aas

--- pages/index.tsx
+++ pages/index.tsx
@@ -84,7 +84,7 @@ const index: React.FC<Home> = ({data}) => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const client = new ApolloClient({
-    uri: "http://18.217.254.134/graphql",
+    uri: "https://app.zahroofvalves.com/graphql",
     cache: new InMemoryCache(),
   });
 
