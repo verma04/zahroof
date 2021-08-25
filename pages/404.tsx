5e98cd39bd4ff027eb4962dca92dd76d8d2ed580
Pankaj Verma <pankaj.k@makein.com>
2021-08-25 16:43:18 +0530

    dsd

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Redirect from '../components/redirect';
+
+
+import React from 'react'
+
+function index() {
+    return (
+        <div>
+         <Redirect to="/" />
+        </div>
+    )
+}
+
+export default index
